95e9fc3d0ba22f9a50c12587635f42cbd651fb91
vla9isla8 <vla9isla8@users.noreply.github.com>
2020-03-22 16:08:36 +0000

    deploy: 97aebc944f66411559c8619e2785766173066c20

---
+++ precache-manifest.986eaee62a63a794954bc7378e9a32c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74c81127c82ac098c458381ecbca3e6",
+    "url": "/reactjs-threejs/index.html"
+  },
+  {
+    "revision": "b65b5676e42b7157363a",
+    "url": "/reactjs-threejs/static/css/main.54716b50.chunk.css"
+  },
+  {
+    "revision": "9951bc028a99f4afc440",
+    "url": "/reactjs-threejs/static/js/2.ed2d6d25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-threejs/static/js/2.ed2d6d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65b5676e42b7157363a",
+    "url": "/reactjs-threejs/static/js/main.fdc8ab7c.chunk.js"
+  },
+  {
+    "revision": "03acede9bbad382daa33",
+    "url": "/reactjs-threejs/static/js/runtime-main.e450d16a.js"
+  }
+]);
